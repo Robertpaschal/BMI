7a3334f9fdeab08365bd226d6d458c27fc23e1d3
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-09-29 04:48:26 +0100

    deploy prep

--- backend/app.js
+++ backend/app.js
@@ -41,8 +41,14 @@ const PORT = process.env.PORT;
 const HOST = process.env.HOSTNAME;
 const ENV = process.env.NODE_ENV;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://${HOST}:${PORT} in ${ENV} mode`);
     console.log(`Swagger docs available at http://${HOST}:${PORT}/api-docs in ${ENV} mode`);
+    if (process.env.NODE_ENV !== 'production') {
+        console.log(`Server is running on http://${HOST}:${PORT} in ${process.env.NODE_ENV} mode`);
+        console.log(`Swagger docs available at http://${HOST}:${PORT}/api-docs in ${ENV} mode`);
+    } else {
+        console.log(`Server is running at https://${HOST} in production mode`);
+        console.log(`Swagger docs available at https://${HOST}/api-docs in production mode`);
+    }
 });
 
 module.exports = app;
